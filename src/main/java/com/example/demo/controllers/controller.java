e847004e05cf237c3c04ae79fb77fac435cf249b
OscarRAmirez <oerm16@gmail.com>
2020-06-20 19:01:07 -0500

    Avance

--- src/main/java/com/example/demo/controllers/controller.java
+++ src/main/java/com/example/demo/controllers/controller.java
@@ -8,23 +8,48 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.demo.service.IFactura_Prueba;
 
-
 @Controller
 @SessionAttributes("Cliente")
 public class controller {
-	
+
 	@Autowired
 	IFactura_Prueba FacturaPrueba;
-	
-	@GetMapping({ "/", "/listado" })
+
+	@GetMapping({ "/listadoClientes" })
 	public String Listar(Model model) {
-		model.addAttribute("titulo", "Listado de Productos");
+		model.addAttribute("titulo", "Listado de Clientes");
 		model.addAttribute("Listado", FacturaPrueba.findall());
-			
+
 		return "listado";
 
 	}
 
-	
-	
+	@GetMapping({ "/Articulos" })
+	public String Articulos(Model model) {
+		model.addAttribute("titulo", "Listado de Articulos");
+		return "Articulos";
+
+	}
+
+	@GetMapping({ "/DetalleFacturas" })
+	public String DetalleFacturas(Model model) {
+		model.addAttribute("titulo", "Detalle Facturas");
+		return "DetalleFactura";
+
+	}
+
+	@GetMapping({ "/Factura" })
+	public String Facturas(Model model) {
+		model.addAttribute("titulo", "Factura");
+		return "Factura";
+
+	}
+
+	@GetMapping({ "/Proveedores" })
+	public String Proveedores(Model model) {
+		model.addAttribute("titulo", "Proveedores");
+		return "Proveedores";
+
+	}
+
 }
